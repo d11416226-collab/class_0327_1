67ef54961d6743ece4426c0633a536829ad6c44b
d11416226-collab <d11416226@gs.takming.edu.tw>
2026-03-27 09:49:26 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Test
+{
+	private int Mid;
+	private int Final;
+
+	public int MyMid (get;) =98;
+
+	public int MyFinal
+	{
+		get
+		{
+			return Final;
+		}
+		set
+		{
+			if (value > 0 || value < 100)
+			{
+				Final = value;
+			}
+			else
+			{
+				Final = 0;
+			}
+		}
+	}
+
+	}
+}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace class_0327_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Test std = new Test();
+
+            //std.MyMid = 92;
+            std.MyFinal = 78;
+
+            Console.WriteLine(std.MyMid. ToString());
+            Console.WriteLine(std.MyFinal.ToString());
+        }
+    }
+}
